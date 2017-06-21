ab1ca047e8adf55f099f492cef66fb2e6c14ac92
Keegan Rush <thecodedself@gmail.com>
2017-06-21 06:26:15 +0200

    Initial Commit

---
+++ HushTime/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HushTime
+//
+//  Created by Keegan Rush on 2017/06/21.
+//  Copyright © 2017 TheCodedSelf. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HushTime/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HushTime
+//
+//  Created by Keegan Rush on 2017/06/21.
+//  Copyright © 2017 TheCodedSelf. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
